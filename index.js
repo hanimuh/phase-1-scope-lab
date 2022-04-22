f23f354b1d4c1b59fcecc2833b6d95afb1f13ac1
Hani <md.hani@hotmail.com>
2022-04-23 01:26:17 +0300

    commit tree c7c86869b9465c1aee524794f7ab6b88260245db

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName= 'bob';
+
+function upperCaseCustomerName(){
+    return customerName= customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer= "not bob"
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer= "maybe bob";
+}
+
+const leastFavoriteCustomer= "Mike"
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer= "not mike"
+}
